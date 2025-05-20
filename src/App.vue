790847476a4368edf4f1fd59d697fe0f25ddc98e
JohnSanusi <johnsanusi18@gmail.com>
2025-05-20 21:44:30 +0100

    Initialised GHIT site

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterView } from "vue-router";
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+<style>
+html {
+  scroll-behavior: smooth;
+  scroll-padding-top: 100px;
+}
+</style>
