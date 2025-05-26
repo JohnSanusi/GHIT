44fd57061fb21d1b5d12230472000bf5345cf0ac
JohnSanusi <johnsanusi18@gmail.com>
2025-05-26 16:13:03 +0100

    Update Navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -60,7 +60,7 @@ let Links = [
           </svg>
 
           <span
-            class="absolute top-0 right-1 flex justify-center items-center bg-gray-400 text-white text-base font-bold cursor-pointer rounded-full w-5 h-5"
+            class="absolute top-0 right-0 flex justify-center items-center bg-gray-400 text-white text-base font-bold cursor-pointer rounded-full w-5 h-5"
             >{{ count }}
           </span>
         </RouterLink>
@@ -72,14 +72,14 @@ let Links = [
         </RouterLink>
         <RouterLink
           to="signup"
-          class="text-white hidden tracking-widest bg-blue-500 hover:bg-blue-600 cursor-pointer focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-3 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 md:flex lg:flex"
+          class="text-white hidden tracking-widest bg-blue-500 hover:bg-blue-600 cursor-pointer focus:ring-4 focus:outline-none focus:ring-blue-300 md:flex lg:flex font-medium rounded-lg text-sm px-4 py-3 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
           Get Started
         </RouterLink>
 
         <button
           type="button"
-          class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden bg-gray-100 outline-none ring-2  ring-gray-600"
+          class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden bg-gray-100 outline-none ring-2 ring-gray-600"
           aria-controls="navbar-sticky"
           aria-expanded="false"
           @click="toggleNavOpen"
@@ -122,51 +122,5 @@ let Links = [
       </div>
     </div>
   </nav>
-  <!--  <nav
-    class="bg-white/20 fixed w-full z-20 top-0 start-0 shadow-md backdrop-blur-md hidden"
-    data-aos="fade-down"
-    data-aos-duration="1000"
-  >
-    <div
-      class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4"
-    >
-      <RouterLink to="" class="flex items-center space-x-3 rtl:space-x-reverse">
-        <img
-          src="../../public/logo.png"
-          class="w-16 h-16 md:w-20 md:h-20 lg:w-20 lg:h-20"
-          alt=""
-        />
-      </RouterLink>
-      <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-        <RouterLink
-          to="/login"
-          class="text-black tracking-widest border border-black-[1.8px] font-medium rounded-lg text-sm px-4 py-3 text-center mr-3 cursor-pointer hover:shadow-xl"
-        >
-          Login
-        </RouterLink>
-        <RouterLink
-          to="signup"
-          class="text-white tracking-widest bg-blue-500 hover:bg-blue-600 cursor-pointer focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-3 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        >
-          Get started
-        </RouterLink>
-      </div>
-      <div
-        class="items-center justify-between w-full md:flex md:w-auto md:order-1"
-        id="navbar-sticky"
-      >
-        <ul
-          class="flex flex-row p-4 md:p-0 mt-4 font-medium md:space-x-8 rtl:space-x-reverse"
-        >
-          <li v-for="link in Links" :key="link.to">
-            <a
-              :href="link.to"
-              class="block py-2 px-3 text-black rounded-sm hover:text-blue-500 md:p-0"
-              >{{ link.name }}</a
-            >
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav> -->
+  
 </template>
