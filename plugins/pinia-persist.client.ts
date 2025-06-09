be1eaed91f8f9862be9d82ca08ee44a79b51aa33
JohnSanusi <johnsanusi18@gmail.com>
2025-06-09 12:59:13 +0100

    pinia persisted state

--- plugins/pinia-persist.client.ts
+++ plugins/pinia-persist.client.ts
@@ -1,9 +1,6 @@
 import { defineNuxtPlugin } from "#app";
-import { createPinia } from "pinia";
 import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 
-export default defineNuxtPlugin((nuxtApp) => {
-  const pinia = createPinia();
-  pinia.use(piniaPluginPersistedstate);
-  nuxtApp.vueApp.use(pinia);
+export default defineNuxtPlugin(({ $pinia }) => {
+  $pinia.use(piniaPluginPersistedstate);
 });
